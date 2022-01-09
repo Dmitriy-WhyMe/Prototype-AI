46207df2e10fa50e38d3709aebb3cb2839e72233
Dmitriy <dima.ivahnoff@mail.ru>
2022-01-09 14:05:13 +0300

    v1.3

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let express = require('express');
 let bodyParser = require('body-parser');
+const brain = require('brain.js');
 
 let app = express();
 let urlencodeParser = bodyParser.urlencoded({ extended: false });
@@ -16,7 +17,6 @@ let jsObjectAi = JSON.parse(file);
 //Form
 app.post('/add', urlencodeParser, function(req, res) {
     if(!req.body) return res.sendStatus(400);
-    console.log(req.body);
     let jsonData = fs.readFileSync('AI.json' , 'utf8');
     let all = jsonData.substring(0, jsonData.length-1)+', '+JSON.stringify(req.body)+"]";
     console.log(all);
@@ -27,13 +27,61 @@ app.post('/add', urlencodeParser, function(req, res) {
     res.render('add');
 });
 
+app.use('/test_1', bodyParser.urlencoded({
+    extended: true
+}));
+
+let length_json = Object.keys(jsObjectAi).length;
+app.post('/test_1', urlencodeParser, function(req, res) {
+    if(!req.body) return res.sendStatus(400);
+    console.log(req.body);
+    let inner_text = req.body.data_text;
+    let count_iterations = req.body.data_iterations;
+    console.log(inner_text);
+    console.log(count_iterations);
+    net = new brain.recurrent.LSTM()
+    let length_json = Object.keys(jsObjectAi).length;
+    console.log(length_json);
+    let big_data =[];
+    let key_input = 'input';
+    let key_output = 'output';
+    let counter = 0;
+
+    //Получение данных из Json файла
+    while (counter < length_json) {
+        let obj = {};
+        obj[key_input] = jsObjectAi[counter].input;
+        obj[key_output] = jsObjectAi[counter].output;
+        big_data.push(obj);
+        counter++;
+        console.log(obj);
+    }
+
+    //learning
+    net.train(big_data, {
+        iterations: count_iterations,
+        log: details => console.log(details),
+        errorThresh: 0.010
+    });
+    console.log(big_data);
+    
+    //Передача полученного значения
+    output = net.run(inner_text);
+
+    //Output
+    console.log(output)
+    res.render('test_1', {length_json: length_json, output: output});
+});
+
+let output = " ";
 
 //Server
 app.listen(3000);
-var myVar = 1;
 app.get('/', function (req, res) {
-    res.render('test_1', {data: jsObjectAi});
+    res.render('test_1', {length_json: length_json, output: output});
 });
+
+
 
 app.get('/:name', function(req, res) {
     if(req.params.name === 'add') {
@@ -41,7 +89,8 @@ app.get('/:name', function(req, res) {
     } else if(req.params.name === 'view') {
         res.render('view', {data: jsObjectAi});
     } else if(req.params.name === 'test_1') {
-        res.render('test_1', {data: jsObjectAi});
+
+        res.render('test_1', {length_json: length_json, output: output});
     } else {
         res.sendFile(__dirname + '/404.html');
     }  
--- public/js/module/common.js
+++ public/js/module/common.js
@@ -1,10 +1,3 @@
-const fs = require('fs');
-let file = fs.readFileSync('AI.json', 'utf8');
-let mydata = JSON.parse(file);
-let length_json = Object.keys(mydata).length;
-const learningCounter = document.querySelector('#learning-counter');
-learningCounter.innerHTML = ("В данный момент в базе находится: " + length_json + " фраз");
-
 (function ($) {
 	$(document).ready(function () {
         //Materialize
